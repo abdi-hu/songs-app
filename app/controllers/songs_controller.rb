bff5823a2252f559a62391a9bcb6878e40b93515
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-04-22 15:58:43 -0400

    adds create controller and private method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,30 @@
 class SongsController < ApplicationController
 
     def index
-        render json: Song.all
+        songs = Song.all
+        render json: {status: 200, songs: songs}
     end
-    
+
+    def show
+        song = Song.find(params[:id])
+        render json: {status: 200, song: song}
+    end
+
+    def create
+        song = Song.new(song_params)
+
+        if song.save
+            render(status: 201, json: { song: song })
+        else
+            # Unprocessable Entity
+            render(status: 422, json: { song: song })
+        end
+    end
+
+    private # Any methods below here. Private methods are not exposed. Helper methods that do not correspond to routes also belong here.
+
+    def song_params
+        # Returns a sanitized hash of the params with nothing extra
+        params.required(:song).permit(:title, :artist_name, :artwork)
+    end
 end
